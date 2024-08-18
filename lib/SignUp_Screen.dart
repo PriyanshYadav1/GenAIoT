6860b323fd337df1fd498125cb18dd0dfd1f61ef
PriyanshYadav1 <yadavpriayansh200@gmail.com>
2024-08-18 15:29:27 +0530

    Overflow and Navigational issues upto home page solved
    Signed-off-by: PriyanshYadav1 <yadavpriayansh200@gmail.com>

--- lib/SignUp_Screen.dart
+++ lib/SignUp_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:genaiot/login_Screen.dart';
 
 class signUp_Screen extends StatelessWidget {
   //Controllers for fields
@@ -21,92 +22,107 @@ class signUp_Screen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(80),
               child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/images/logo-no-background.png'),
-                    SizedBox(
-                      height: 80,
-                    ),
-                    const Text(
-                      "Sign Up",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Roboto',
-                          fontSize: 30),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(30),
+                child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/logo-no-background.png'),
+                      SizedBox(
+                        height: 80,
+                      ),
+                      const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Roboto',
+                            fontSize: 30),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
                         ),
-                        filled: true,
-                        fillColor: Colors.white,
+                        controller: signUP_EmailController,
                       ),
-                      controller: signUP_EmailController,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: const BorderSide(color: Colors.white)),
-                        filled: true,
-                        fillColor: Colors.white,
+                      const SizedBox(
+                        height: 20,
                       ),
-                      controller: signUP_PasswordController,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        hintText: ' Retype Password',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: const BorderSide(color: Colors.white)),
-                        filled: true,
-                        fillColor: Colors.white,
+                      TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide:
+                                  const BorderSide(color: Colors.white)),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        controller: signUP_PasswordController,
                       ),
-                      controller: signUP_PasswordController,
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Register',
-                        style: TextStyle(color: Colors.black),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white),
+                      TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: ' Retype Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide:
+                                  const BorderSide(color: Colors.white)),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        controller: signUP_PasswordController,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    )
-                  ],
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => login_Screen()));
+                        },
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => login_Screen()));
+                        },
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/passkey_Screen.dart
+++ lib/passkey_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:genaiot/home.dart';
 import 'package:genaiot/login_Screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -33,67 +34,80 @@ class _passkey_ScreenState extends State<passkey_Screen> {
             Padding(
               padding: const EdgeInsets.all(80),
               child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/images/logo-no-background.png'),
-                    SizedBox(
-                      height: 80,
-                    ),
-                    const Text(
-                      "Welcome Back !",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Roboto',
-                          fontSize: 30),
-                    ),
-                    const SizedBox(
-                      height: 60,
-                    ),
-                    TextField(
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        hintText: 'Pass Key',
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(30),
+                child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/logo-no-background.png'),
+                      SizedBox(
+                        height: 80,
+                      ),
+                      const Text(
+                        "Welcome Back !",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Roboto',
+                            fontSize: 30),
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: 'Pass Key',
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
                         ),
-                        filled: true,
-                        fillColor: Colors.white,
+                        controller: passKeyController,
                       ),
-                      controller: passKeyController,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        var passkey = passKeyController.text.toString();
-                        var prefs = await SharedPreferences.getInstance();
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          var passkey = passKeyController.text.toString();
+                          var prefs = await SharedPreferences.getInstance();
 
-                        prefs.setString("passkey", passkey);
-                      },
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.black),
+                          prefs.setString("passkey", passkey);
+
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage()));
+                        },
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(color: Colors.black),
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Login with credentials',
-                        style: TextStyle(color: Colors.white),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 55,
-                    )
-                  ],
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => login_Screen()));
+                        },
+                        child: const Text(
+                          'Login with credentials',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 55,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
