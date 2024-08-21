1c12f33d6d101a178869fd486694c351257adb5f
Apurva <apurvaroy111@gmail.com>
2024-08-21 17:11:09 +0530

    model(just for example)

---
+++ lib/models/assets_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Assets_ListItem {
+  final String title;
+  final IconData icon;
+
+  Assets_ListItem({required this.title, required this.icon});
+}
